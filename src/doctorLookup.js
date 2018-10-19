b6539cb91419e419206c58baec6e2875564188be
Chris Crow <chrismcrow@gmail.com>
2018-10-19 09:16:00 -0700

    refactor api to use javascript method

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,3 +1,24 @@
+export class Search {
+  lookup(name, query) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.API_KEY}&location=or-portland&query=${query}&name=${name}`;
+      request.onload = function() {
+        console.log("LOADED");
+        if(this.status === 200){
+          console.log(request.response);
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+
+
 export class Doctor {
   constructor(firstName, lastName, address, phone, newPatients) {
     this.firstName = firstName;
--- src/main.js
+++ src/main.js
@@ -2,48 +2,83 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// const Promise = require('es6-promise').Promise;
+import { Doctor, Search } from './doctorLookup.js';
 
 $(document).ready(function() {
   $("form#doctorLookup").submit(function(event) {
+    event.preventDefault();
     let doctorName = $("input#doctorName").val();
     let query = $("input#query").val();
 
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${API_KEY}&location=or-portland&query=${query}&name=${doctorName}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $("results-table").empty();
+    let newSearch = new Search();
+    let promise = newSearch.lookup(doctorName, query);
 
-        let body = JSON.parse(response);
-        for (let i = 0; i < body.data.length; i++) {
-          let firstName = body.data[i].profile.first_name;
-          let lastName = body.data[i].profile.last_name;
-          let address =
-            `${body.data[i].practices[0].visit_address.street}
-            ${body.data[i].practices[0].visit_address.street2}
-            ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}`;
-          let phone = body.data[i].practices[0].phones.number;
-          let newPatients = body.data[i].practices[0].accepts_new_patients;
-          let doctor = new Doctor(firstName, lastName, address, phone, newPatients);
+    promise.then(function(response) {
+      $("results-table").empty();
 
-          $('#results-table').append(
-            `<tr>
-              <td>${doctor.firstName}</td>
-              <td>${doctor.lastName}</td>
-              <td>${doctor.address}</td>
-              <td>${doctor.phone}</td>
-              <td>${doctor.newPatients}</td>
-            </tr>`
-          );
-        }
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.");
+      let body = JSON.parse(response);
+      for (let i = 0; i < body.data.length; i++) {
+        let firstName = body.data[i].profile.first_name;
+        let lastName = body.data[i].profile.last_name;
+        let address =
+          `${body.data[i].practices[0].visit_address.street}
+          ${body.data[i].practices[0].visit_address.street2}
+          ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}`;
+        let phone = body.data[i].practices[0].phones.number;
+        let newPatients = body.data[i].practices[0].accepts_new_patients;
+        let doctor = new Doctor(firstName, lastName, address, phone, newPatients);
+
+        $('#results-table').append(
+          `<tr>
+            <td>${doctor.firstName}</td>
+            <td>${doctor.lastName}</td>
+            <td>${doctor.address}</td>
+            <td>${doctor.phone}</td>
+            <td>${doctor.newPatients}</td>
+          </tr>`
+        );
       }
-    });
-  })
+    }, function(error) {
+      $('#errors').text("There was an error processing your request. Please try again.");
+    }
+
+
+
+    // $.ajax({
+    //   url: `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.API_KEY}&location=or-portland&query=${query}&name=${doctorName}`,
+    //   type: 'GET',
+    //   data: {
+    //     format: 'json'
+    //   },
+    //   success: function(response) {
+    //     $("results-table").empty();
+    //
+    //     let body = JSON.parse(response);
+    //     for (let i = 0; i < body.data.length; i++) {
+    //       let firstName = body.data[i].profile.first_name;
+    //       let lastName = body.data[i].profile.last_name;
+    //       let address =
+    //         `${body.data[i].practices[0].visit_address.street}
+    //         ${body.data[i].practices[0].visit_address.street2}
+    //         ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}`;
+    //       let phone = body.data[i].practices[0].phones.number;
+    //       let newPatients = body.data[i].practices[0].accepts_new_patients;
+    //       let doctor = new Doctor(firstName, lastName, address, phone, newPatients);
+    //
+    //       $('#results-table').append(
+    //         `<tr>
+    //           <td>${doctor.firstName}</td>
+    //           <td>${doctor.lastName}</td>
+    //           <td>${doctor.address}</td>
+    //           <td>${doctor.phone}</td>
+    //           <td>${doctor.newPatients}</td>
+    //         </tr>`
+    //       );
+    //     }
+    //   },
+    //   error: function() {
+    //     $('#errors').text("There was an error processing your request. Please try again.");
+    //   }
+    // });
+  });
 });
